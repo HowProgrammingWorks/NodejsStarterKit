6a9a7513d0d380fecd6f0b016380bf2269b8cff5
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-03-25 03:51:12 +0200

    Add config example

---
+++ config/database.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  host: '127.0.0.1',
+  port: 5432,
+  database: 'application',
+  user: 'marcus',
+  password: 'marcus',
+};
---
+++ config/server.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+  host: '127.0.0.1',
+  port: 8000,
+};
