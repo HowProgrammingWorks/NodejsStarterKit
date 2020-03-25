5df121f82c1ff0685aa2e0b9125034c9ca60eb81
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-03-25 23:13:13 +0200

    Load application in current directory

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const Application = require('./lib/application.js');
+
+const cwd = process.cwd();
+const application = new Application(cwd);
+application.on('started', () => {
+  console.log('Application loaded');
+});
