5efe150894eaa9727f9a4d88065c4936bd08c5dd
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-03-27 01:54:26 +0200

    Make application instance global

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const Application = require('./lib/application.js');
 
 const cwd = process.cwd();
 const application = new Application(cwd);
+global.application = application;
+
 application.on('started', () => {
   console.log('Application loaded');
 });
