43e597027198b5ed457bc5eaad63d3ed7bb7889e
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-04-01 22:12:39 +0300

    Remove old method examples

--- api/read.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = async name => {
-  const shape = application.state.get(name);
-  if (!shape) return 'Shape is not found';
-  return shape;
-};
--- api/rect.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { Rect } = application.domain;
-
-module.exports = async (name, x1, y1, x2, y2) => {
-  const rect = new Rect(x1, y1, x2, y2);
-  application.state.set(name, rect);
-  return 'ok';
-};
