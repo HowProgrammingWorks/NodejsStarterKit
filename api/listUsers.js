22b41bcca608e1ddb87076659fac7d73d2da4d68
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-04-01 22:13:34 +0300

    Add new method examples

---
+++ api/listUsers.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = async () => {
+  const fields = ['Id', 'Login', 'FullName'];
+  const data = await application.db.select('SystemUser', fields);
+  return { result: 'success', data };
+};
---
+++ api/registerUser.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = async (login, password, fullName) => {
+  const user = {
+    Login: login,
+    Password: password,
+    FullName: fullName,
+  };
+  const id = application.db.insert('SystemUser', user);
+  if (!id) throw new Error('Can not register user');
+  return { result: 'success' };
+};
