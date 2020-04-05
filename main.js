b5388f983b25208bc6db35fa1e66602bcd4a118a
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-04-05 15:42:27 +0300

    Remove unneeded domain model example

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 'use strict';
 
 const Application = require('./lib/application.js');
-const { Rect } = require('./domain/rect.js');
 
-const domain = { Rect };
-const application = new Application({ domain });
+const application = new Application();
 
 application.on('started', () => {
   application.logger.log('Application loaded');
