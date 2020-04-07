d12e8163bac12b5d8f274d79a96ecc540c8ceba5
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-04-07 11:44:22 +0300

    Add configuration parameters
    Refs: https://github.com/HowProgrammingWorks/NodejsStarterKit/issues/15
    Refs: https://github.com/HowProgrammingWorks/NodejsStarterKit/issues/16
    Refs: https://github.com/HowProgrammingWorks/NodejsStarterKit/issues/17

--- config/server.js
+++ config/server.js
@@ -4,8 +4,14 @@ module.exports = {
   host: '127.0.0.1',
   transport: 'http',
   port: 8000,
+  timeout: 5000,
+  concurrency: 1000,
+  queue: {
+    size: 2000,
+    timeout: 3000,
+  },
   api: {
     transport: 'http',
     ports: [8001, 8002, 8003, 8004],
-  }
+  },
 };
