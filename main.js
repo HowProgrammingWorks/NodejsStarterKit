353c6102118a1a275979a3bc6b1870fc0d0894d3
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-04-09 21:29:55 +0300

    Run single port server in single wotker thread
    Refs: https://github.com/HowProgrammingWorks/NodejsStarterKit/issues/5

--- main.js
+++ main.js
@@ -1,28 +1,9 @@
 'use strict';
 
-const worker = require('worker_threads');
+const { Worker } = require('worker_threads');
 
-const Application = require('./lib/application.js');
-
-const application = new Application(worker);
-
-application.on('started', () => {
-  application.logger.log('Application loaded');
-});
+const worker = new Worker('./worker.js');
 
 process.on('SIGINT', async () => {
-  if (application.finalization) return;
-  console.log();
-  application.logger.log('Graceful shutdown');
-  await application.shutdown();
-  application.logger.log('Bye');
-  process.exit(0);
+  worker.postMessage({ name: 'stop' });
 });
-
-const logError = err => {
-  application.logger.error(err.stack);
-};
-
-process.on('uncaughtException', logError);
-process.on('warning', logError);
-process.on('unhandledRejection', logError);
---
+++ worker.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const worker = require('worker_threads');
+
+const Application = require('./lib/application.js');
+
+const application = new Application(worker);
+
+application.on('started', () => {
+  application.logger.log(`Application started in worker ${worker.threadId}`);
+});
+
+worker.parentPort.on('message', async message => {
+  if (message.name === 'stop') {
+    if (application.finalization) return;
+    console.log();
+    application.logger.log('Graceful shutdown');
+    await application.shutdown();
+    process.exit(0);
+  }
+});
+
+const logError = err => {
+  application.logger.error(err.stack);
+};
+
+process.on('uncaughtException', logError);
+process.on('warning', logError);
+process.on('unhandledRejection', logError);
