ca94671a9b4dc4933aa52fcbd8e1910c83a857d4
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-04-18 22:32:20 +0300

    Generate multiple domain and IP certificate

--- cert/generate.sh
+++ cert/generate.sh
@@ -1,2 +1,5 @@
 cd "$(dirname "$0")"
-openssl req -x509 -nodes -new -sha256 -days 1024 -newkey rsa:2048 -keyout key.pem -out cert.pem -subj '/CN=localhost'
+openssl genrsa -out key.pem 2048
+openssl req -new -out self.pem -key key.pem -subj '/CN=localhost'
+openssl req -text -noout -in self.pem
+openssl x509 -req -days 1024 -in self.pem -signkey key.pem -out cert.pem -extfile generate.ext
