6d8f6934d6e766f97078876be3f3c60a399cb1df
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-04-21 23:15:13 +0300

    Fix password hash for demo login marcus
    PR-URL: https://github.com/HowProgrammingWorks/NodejsStarterKit/pull/46

--- db/data.sql
+++ db/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO SystemUser VALUES
   (1, 'admin', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$XcD5Zfk+BVIGEyiksBjjy9LL42AFOOqlhEB650woECs$3CNOs25gOVV8AZMYQc6bFnrYdM+3xP996shxJEq5LxGt4gs1g9cocZmi/SYg/H16egY4j7qxTD/oygyEI80cgg'),
-  (2, 'marcus', '67108864$BmPFipv2QU9kpC6S/wEBCfw3/BYijrADK07y8nP+Gn0$W291hVMbm9qHUJbCOql1uy3cGBYqeig8Y7F2j+EZGInle/o9mq84pw0A9A/eFVWU6mI9eTrJiRFS3DQoc3D8Jw'),
+  (2, 'marcus', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$aGKuH5D2zndi6zFu74/rEj5m3u5kRh5b+QXYfKrhAU8$257up1h/3R9CoxH2382zX0+kbxRPrd+GwzJIxYI+K+gBYCcLrA8Z6wv7lSwLElfbDTJRgUhQJFhMT1tpp5AJxw'),
   (3, 'user', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$z5uf2xGdpgq5v2sZbgh36QoZG9CDmGmJUNJkrs1zs2w$3s3x22k4Td0jkup4WduFQGFVjrFKHjN1WV9k8/Bh3DKI58Wrlo/D4Js9j/DiskwI8rltDd8pF15JylivFu2T0g'),
   (4, 'iskandar', '$scrypt$N=32768,r=8,p=1,maxmem=67108864$EfcPTou2sTms7Esp4lsbJddN2RLAqqZUhP6sflwT7KU$FJiY0ad+qeNtZyFa0sXfQfeSIDS5HYS8wMk2/gtUlqy5vBddzVgKQYDqF5lKMNCm7IpOaYUZtRv7BQbxbVgkYg');
 
