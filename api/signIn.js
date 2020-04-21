ebe017903215507dc7ddb6d311315529ff013d9b
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-04-21 23:48:26 +0300

    Fix password hash validation
    Refs: https://github.com/HowProgrammingWorks/NodejsStarterKit/issues/39
    PR-URL: https://github.com/HowProgrammingWorks/NodejsStarterKit/pull/46

--- api/signIn.js
+++ api/signIn.js
@@ -2,7 +2,7 @@ async ({ login, password }) => {
   const [user] = await application.db.select(
     'SystemUser', ['Password'], { login }
   );
-  const hash = user && user.Password || api.security.defaultHash;
+  const hash = user ? user.password : undefined;
   const verified = await api.security.validatePassword(password, hash);
   if (!user || !verified) throw new Error('Incorrect login or password');
   console.log(`Logged user: ${login}`);
