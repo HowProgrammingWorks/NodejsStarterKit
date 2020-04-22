b771ad97c08022762480091f9ab1999dab7c0ff0
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-04-22 23:55:56 +0300

    Test: Config class
    Refs: https://github.com/HowProgrammingWorks/NodejsStarterKit/issues/11
    PR-URL: https://github.com/HowProgrammingWorks/NodejsStarterKit/pull/48

--- test/unit.config.js
+++ test/unit.config.js
@@ -1,7 +1,18 @@
 'use strict';
 
+const path = require('path');
 const assert = require('assert').strict;
 
 const Config = require('../lib/config.js');
 
 assert(Config);
+
+const PATH = process.cwd();
+const configPath = path.join(PATH, 'config');
+
+const config = new Config(configPath);
+config.on('loaded', () => {
+  assert(config.sections);
+  assert(config.sections.database);
+  assert(config.sections.server);
+});
