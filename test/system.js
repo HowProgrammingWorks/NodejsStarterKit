9844ca70f9311058fce9fbf1ddfa6483c3bcd438
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-04-22 04:48:59 +0300

    Add HTTP request runner to local server for testing
    Refs: https://github.com/HowProgrammingWorks/NodejsStarterKit/issues/11

--- test/system.js
+++ test/system.js
@@ -1,8 +1,13 @@
 'use strict';
 
+const http = require('http');
+const assert = require('assert').strict;
 const { Worker } = require('worker_threads');
-const worker = new Worker('./worker.js');
 
+const worker = new Worker('./worker.js');
+const config = require('../config/server.js');
+
+const START_TIMEOUT = 500;
 const TEST_TIMEOUT = 3000;
 
 console.log('System test started');
@@ -13,3 +18,52 @@ setTimeout(async () => {
 worker.on('exit', () => {
   console.log('System test finished');
 });
+
+const tasks = [
+  { get: '/' },
+  { get: '/websocket.js' },
+  {
+    post: '/api/signIn',
+    data: { login: 'marcus', password: 'marcus' }
+  }
+];
+
+const getRequest = task => {
+  const request = {
+    host: config.host,
+    port: config.ports[0],
+    agent: false
+  };
+  if (task.get) {
+    request.method = 'GET';
+    request.path = task.get;
+  } else if (task.post) {
+    request.method = 'POST';
+    request.path = task.post;
+  }
+  if (task.data) {
+    task.data = JSON.stringify(task.data);
+    request.headers = {
+      'Content-Type': 'application/json',
+      'Content-Length': task.data.length
+    };
+  }
+  return request;
+};
+
+setTimeout(() => {
+  tasks.forEach(task => {
+    const name = task.get || task.post;
+    console.log('HTTP request ' + name);
+    const request = getRequest(task);
+    const req = http.request(request);
+    req.on('response', res => {
+      assert.equal(res.statusCode, 200);
+    });
+    req.on('error', err => {
+      assert.fail(err);
+    });
+    if (task.data) req.write(task.data);
+    req.end();
+  });
+}, START_TIMEOUT);
