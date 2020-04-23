30e70f7e9709814bd547706ba01edc8e4d1a2f7b
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-04-23 03:38:45 +0300

    Test: Database class
    Refs: https://github.com/HowProgrammingWorks/NodejsStarterKit/issues/11
    PR-URL: https://github.com/HowProgrammingWorks/NodejsStarterKit/pull/48

--- test/unit.db.js
+++ test/unit.db.js
@@ -3,5 +3,47 @@
 const assert = require('assert').strict;
 
 const Database = require('../lib/db.js');
+const databaseConfig = require('../config/database.js');
 
 assert(Database);
+
+const applicationStub = { logger: { log: console.log } };
+
+const database = new Database(databaseConfig, applicationStub);
+
+(async () => {
+  const empty = 'empty';
+  try {
+    const user = { login: empty, password: empty, fullName: empty };
+    const result = await database.insert('SystemUser', user);
+    assert(result);
+    assert.equal(result.rowCount, 1);
+  } catch (err) {
+    assert.fail(err);
+  }
+  try {
+    const fields = ['login', 'password'];
+    const cond = { login: empty };
+    const [record] = await database.select('SystemUser', fields, cond);
+    assert.equal(record.login, empty);
+    assert.equal(record.password, empty);
+  } catch (err) {
+    assert.fail(err);
+  }
+  try {
+    const delta = { password: empty };
+    const cond = { login: empty };
+    const result = await database.update('SystemUser', delta, cond);
+    assert.equal(result.rowCount, 1);
+  } catch (err) {
+    assert.fail(err);
+  }
+  try {
+    const cond = { login: empty };
+    const result = await database.delete('SystemUser', cond);
+    assert.equal(result.rowCount, 1);
+  } catch (err) {
+    assert.fail(err);
+  }
+  database.close();
+})();
