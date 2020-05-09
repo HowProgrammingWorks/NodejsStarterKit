b857d0cfc1c3b72eb3f3129cb7d4f66dfd340929
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-05-09 23:58:04 +0300

    Refactor dependencies
    Closes: https://github.com/HowProgrammingWorks/NodejsStarterKit/issues/85

--- api/registerUser.js
+++ api/registerUser.js
@@ -1,5 +1,5 @@
 async ({ login, password, fullName }) => {
-  const hash = await api.security.hashPassword(password);
+  const hash = await application.security.hashPassword(password);
   await application.auth.registerUser(login, hash, fullName);
   return { result: 'success' };
 };
--- api/signIn.js
+++ api/signIn.js
@@ -1,7 +1,7 @@
 async ({ login, password }) => {
   const user = await application.auth.getUser(login);
   const hash = user ? user.password : undefined;
-  const verified = await api.security.validatePassword(password, hash);
+  const verified = await application.security.validatePassword(password, hash);
   if (!user || !verified) throw new Error('Incorrect login or password');
   console.log(`Logged user: ${login}`);
   return { result: 'success', userId: user.id };
