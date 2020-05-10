045c16e4d02c942da9685153f8d846e08892cc7f
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-05-10 04:55:04 +0300

    Restart worker on exitCode !== 0
    Closes: https://github.com/HowProgrammingWorks/NodejsStarterKit/issues/76
    PR-URL: https://github.com/HowProgrammingWorks/NodejsStarterKit/pull/90

--- server.js
+++ server.js
@@ -8,23 +8,29 @@ const Config = require('./lib/config.js');
 const PATH = process.cwd();
 const CFG_PATH = path.join(PATH, 'config');
 
-const workers = [];
-
 (async () => {
   const config = await new Config(CFG_PATH);
   const { sections } = config;
-  for (let i = 0; i < sections.server.ports.length; i++) {
+  const count = sections.server.ports.length;
+  const workers = new Array(count);
+
+  const start = id => {
     const worker = new Worker('./worker.js');
-    workers.push(worker);
-  }
+    workers[id] = worker;
+    worker.on('exit', code => {
+      if (code !== 0) start(id);
+    });
+  };
+
+  for (let id = 0; id < count; id++) start(id);
+
+  const stop = async () => {
+    console.log();
+    for (const worker of workers) {
+      worker.postMessage({ name: 'stop' });
+    }
+  };
+
+  process.on('SIGINT', stop);
+  process.on('SIGTERM', stop);
 })();
-
-const exit = async () => {
-  console.log();
-  for (const worker of workers) {
-    worker.postMessage({ name: 'stop' });
-  }
-};
-
-process.on('SIGINT', exit);
-process.on('SIGTERM', exit);
