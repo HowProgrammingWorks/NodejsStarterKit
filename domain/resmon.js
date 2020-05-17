360fa1ce08f7078b88533d59d5c4306b3d69b347
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-05-18 01:04:56 +0300

    Remove init section, use domain instead
    PR-URL: https://github.com/HowProgrammingWorks/NodejsStarterKit/pull/121

--- domain/resmon.js
+++ domain/resmon.js
@@ -10,4 +10,17 @@ const getStatistics = () => {
   return { heapTotal, heapUsed, external, contexts, detached };
 };
 
-module.exports = { getStatistics };
+const startMonitoring = application => {
+  const config = application.config.sections.resmon;
+  setInterval(() => {
+    const stats = application.resmon.getStatistics();
+    const { heapTotal, heapUsed, external, contexts, detached } = stats;
+    const total = application.utils.bytesToSize(heapTotal);
+    const used = application.utils.bytesToSize(heapUsed);
+    const ext = application.utils.bytesToSize(external);
+    application.logger.log(`Heap: ${used} of ${total}, ext: ${ext}`);
+    application.logger.log(`Contexts: ${contexts}, detached: ${detached}`);
+  }, config.interval);
+};
+
+module.exports = { getStatistics, startMonitoring };
