e51e8179203108effc2746e54ceac4e6c8492c19
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-05-18 03:01:43 +0300

    Refactor domain code to be compatible with sandbox
    Refs: https://github.com/HowProgrammingWorks/NodejsStarterKit/issues/122
    Refs: https://github.com/HowProgrammingWorks/NodejsStarterKit/issues/87
    Refs: https://github.com/HowProgrammingWorks/NodejsStarterKit/issues/82
    PR-URL: https://github.com/HowProgrammingWorks/NodejsStarterKit/pull/121

--- domain/resmon.js
+++ domain/resmon.js
@@ -1,26 +1,22 @@
-'use strict';
+({
+  getStatistics() {
+    const { heapTotal, heapUsed, external } = api.process.memoryUsage();
+    const hs = api.v8.getHeapStatistics();
+    const contexts = hs.number_of_native_contexts;
+    const detached = hs.number_of_detached_contexts;
+    return { heapTotal, heapUsed, external, contexts, detached };
+  },
 
-const v8 = require('v8');
-
-const getStatistics = () => {
-  const { heapTotal, heapUsed, external } = process.memoryUsage();
-  const hs = v8.getHeapStatistics();
-  const contexts = hs.number_of_native_contexts;
-  const detached = hs.number_of_detached_contexts;
-  return { heapTotal, heapUsed, external, contexts, detached };
-};
-
-const startMonitoring = application => {
-  const config = application.config.sections.resmon;
-  setInterval(() => {
-    const stats = application.resmon.getStatistics();
-    const { heapTotal, heapUsed, external, contexts, detached } = stats;
-    const total = application.utils.bytesToSize(heapTotal);
-    const used = application.utils.bytesToSize(heapUsed);
-    const ext = application.utils.bytesToSize(external);
-    application.logger.log(`Heap: ${used} of ${total}, ext: ${ext}`);
-    application.logger.log(`Contexts: ${contexts}, detached: ${detached}`);
-  }, config.interval);
-};
-
-module.exports = { getStatistics, startMonitoring };
+  start() {
+    const { interval } = application.resmon.config;
+    api.timers.setInterval(() => {
+      const stats = application.resmon.getStatistics();
+      const { heapTotal, heapUsed, external, contexts, detached } = stats;
+      const total = application.utils.bytesToSize(heapTotal);
+      const used = application.utils.bytesToSize(heapUsed);
+      const ext = application.utils.bytesToSize(external);
+      console.log(`Heap: ${used} of ${total}, ext: ${ext}`);
+      console.log(`Contexts: ${contexts}, detached: ${detached}`);
+    }, interval);
+  }
+});
