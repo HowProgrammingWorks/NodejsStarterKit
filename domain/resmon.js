6764bcec3d252daec88a4cdc76a44c7f945024a3
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-05-18 03:29:37 +0300

    fixup! Refactor domain code to be compatible with sandbox

--- domain/resmon.js
+++ domain/resmon.js
@@ -1,24 +1,22 @@
-'use strict';
+({
+  getStatistics() {
+    const { heapTotal, heapUsed, external } = api.process.memoryUsage();
+    const hs = api.v8.getHeapStatistics();
+    const contexts = hs.number_of_native_contexts;
+    const detached = hs.number_of_detached_contexts;
+    return { heapTotal, heapUsed, external, contexts, detached };
+  },
 
-const getStatistics = () => {
-  const { heapTotal, heapUsed, external } = api.process.memoryUsage();
-  const hs = api.v8.getHeapStatistics();
-  const contexts = hs.number_of_native_contexts;
-  const detached = hs.number_of_detached_contexts;
-  return { heapTotal, heapUsed, external, contexts, detached };
-};
-
-const start = () => {
-  const { config } = application.resmon;
-  setInterval(() => {
-    const stats = application.resmon.getStatistics();
-    const { heapTotal, heapUsed, external, contexts, detached } = stats;
-    const total = application.utils.bytesToSize(heapTotal);
-    const used = application.utils.bytesToSize(heapUsed);
-    const ext = application.utils.bytesToSize(external);
-    application.logger.log(`Heap: ${used} of ${total}, ext: ${ext}`);
-    application.logger.log(`Contexts: ${contexts}, detached: ${detached}`);
-  }, config.interval);
-};
-
-module.exports = { getStatistics, start };
+  start() {
+    const { config } = application.resmon;
+    setInterval(() => {
+      const stats = application.resmon.getStatistics();
+      const { heapTotal, heapUsed, external, contexts, detached } = stats;
+      const total = application.utils.bytesToSize(heapTotal);
+      const used = application.utils.bytesToSize(heapUsed);
+      const ext = application.utils.bytesToSize(external);
+      application.logger.log(`Heap: ${used} of ${total}, ext: ${ext}`);
+      application.logger.log(`Contexts: ${contexts}, detached: ${detached}`);
+    }, config.interval);
+  }
+});
