4f023d12457ebfc90a61312eb8d7c943e6004011
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-07-18 12:18:37 +0300

    Implement simplified metacom client
    Refs: https://github.com/HowProgrammingWorks/NodejsStarterKit/issues/152
    PR-URL: https://github.com/HowProgrammingWorks/NodejsStarterKit/pull/161

--- static/console.js
+++ static/console.js
@@ -1,39 +1,8 @@
-'use strict';
+import { Metacom } from './metacom.js';
 
-// API Builder
-
-const socket = new WebSocket('wss://' + location.host);
-
-const buildAPI = (methods, socket = null) => {
-  const api = {};
-  for (const method of methods) {
-    api[method] = (args = {}) => new Promise((resolve, reject) => {
-      if (socket) {
-        socket.send(JSON.stringify({ method, args }));
-        socket.onmessage = event => {
-          const obj = JSON.parse(event.data);
-          if (obj.result !== 'error') resolve(obj);
-          else reject(new Error(`Status Code: ${obj.reason}`));
-        };
-      } else {
-        fetch(`/api/${method}`, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(args),
-        }).then(res => {
-          const { status } = res;
-          if (status === 200) resolve(res.json());
-          else reject(new Error(`Status Code: ${status}`));
-        });
-      }
-    });
-  }
-  return api;
-};
-
-let api = buildAPI(['status', 'signIn', 'introspection'], socket);
-
-// Console Emulation
+const metacom = new Metacom(location.host);
+const { api } = metacom;
+window.api = api;
 
 const ALPHA_UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const ALPHA_LOWER = 'abcdefghijklmnopqrstuvwxyz';
@@ -276,12 +245,12 @@ function commandLoop() {
 
 const signIn = async () => {
   try {
+    await metacom.load('status', 'signIn', 'introspection');
     await api.status();
   } catch (err) {
     await api.signIn({ login: 'marcus', password: 'marcus' });
   }
-  const methods = await api.introspection();
-  api = buildAPI(methods, socket);
+  await metacom.load('example');
 };
 
 window.addEventListener('load', () => {
